867f102e1e9af8cfea7104260a61bdbc46ea7b96
Mahmoud-Da <mdabbagh6@gmail.com>
2024-03-20 13:55:43 +0900

    finish 7-Client-and-Server-Components

---
+++ 2-Next.js-Fundamentals/next-app/app/components/ProductCard2.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AddToCart from "./AddToCart";
+
+const ProductCard2 = () => {
+  return (
+    <>
+      <AddToCart></AddToCart>
+    </>
+  );
+};
+
+export default ProductCard2;
