43898b4f8c56b9f68a02e8796ddcfd45f9f54afe
Mahmoud-Da <mdabbagh6@gmail.com>
2024-04-05 14:44:40 +0900

    finish Updating-an-Object

--- 5-Building-APIs/next-app/app/api/users/[id]/route.tsx
+++ 5-Building-APIs/next-app/app/api/users/[id]/route.tsx
@@ -16,3 +16,22 @@ export function GET(
     return NextResponse.json({ error: "User not Found" }, { status: 404 });
   return NextResponse.json({ id: 1, name: "Moody" });
 }
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  // Validate the request body
+  // if invalid, return 400 error
+  const body = await request.json();
+  if (!body.name)
+    return NextResponse.json({ error: "Name is Required" }, { status: 400 });
+
+  // fetch the user with the given id
+  // if doesn`t exist, return 404
+  if (params.id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  // update the user
+  // Return the updated user
+  return NextResponse.json({ id: 1, name: body.name });
+}
